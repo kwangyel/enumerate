ccff7270b7cf89220e3f5cc030352f1497caf811
kwangyel <kwangyel@gmail.com>
2020-09-10 01:31:27 +0600

    logging for production

--- api/index.js
+++ api/index.js
@@ -11,11 +11,21 @@ import unitRoutes from './server/routes/unitRoutes'
 import householdRoutes from './server/routes/householdRoutes'
 import shopRoutes from './server/routes/shopRoutes'
 import imageRoutes from './server/routes/imageRoutes'
+import fs from 'fs'
+import util from 'util'
+
 
 config.config()
 
 const app=express()
 const port = process.env.PORT || 8080
+
+if(process.env.NODE_ENV == "production"){
+	var log_file = fs.createWriteStream(__dirname + '/debug.log',{flags: 'w'})
+	console.log = function(d){
+		log_file.write(util.format(d)+'\n');
+	}
+}
 
 //basic starter middlewares
 app.use(cors())
